4516b8694c919c6eb1b1930003c1b0158bb63188
slytomcat <slytomcat@mail.ru>
2020-09-10 01:34:54 +0300

    more tests for queue

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -12,31 +12,69 @@ import (
 func TestNew(t *testing.T) {
 	conf := struct{ QUEUE Config }{}
 	err := tools.GetConfig("../config.json", "", &conf)
-	tools.PanicIf(err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	q, err := NewQueue(&conf.QUEUE)
-	tools.PanicIf(err)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	tdata1 := "test data 1"
-	tdata2 := "test data 2"
-	tdata3 := "test data 3"
+	// tdata2 := "test data 2"
+	// tdata3 := "test data 3"
 
 	err = q.Send(tdata1)
-	tools.PanicIf(err)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	err = q.Send(tdata2)
-	tools.PanicIf(err)
+	// err = q.Send(tdata2)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
 
-	_ = q.Send(tdata3)
+	// _ = q.Send(tdata3)
 
 	data, receipt, err := q.Receive()
-	tools.PanicIf(err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	t.Log(data)
 
-	q.Delete(receipt)
+	err = q.Delete(receipt)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	err = q.Check()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data, receipt, err = q.Receive()
+	if err == nil {
+		t.Fatal("no error when expected")
+	}
+	t.Logf("expected error: %v", err)
+
+	// clearance
 	req, _ := q.q.PurgeQueueRequest(&sqs.PurgeQueueInput{
 		QueueUrl: aws.String(q.queueURL),
 	})
 	req.Send()
+
+	q.q.DeleteQueue(&sqs.DeleteQueueInput{QueueUrl: aws.String(q.queueURL)})
+
+	data, receipt, err = q.Receive()
+	if err == nil {
+		t.Fatal("no error when expected")
+	}
+	t.Logf("expected error: %v", err)
+
+	q, err = NewQueue(&conf.QUEUE)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 }
